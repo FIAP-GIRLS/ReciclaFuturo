dc24690da1be2184979c36569d0f8caeef6422ed
esterquintino <esterqdasilva@outlook.com>
2024-06-27 12:54:01 -0300

    Model Endereço

---
+++ ReciclaFuturo/Models/EnderecoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace ReciclaFuturo.Models
+{
+    public class EnderecoModel
+    {
+
+        public int EnderecoId { get; set; }
+        public string? NomeEndereco { get; set; }
+        public int? NumeroEndereco { get; set; }
+        public string? Bairro { get; set; }
+        public int? Cep { get; set; }
+        public string? Cidade { get; set; }
+        public string? Estado { get; set; }
+        public string? complemento { get; set; }
+
+    }
+}
