1282d4a88cc970df61fea370ebe8f7af010b74d5
esterquintino <esterqdasilva@outlook.com>
2024-06-27 20:37:06 -0300

    Mesclando mudanças da branch remota xUnit

--- ReciclaFuturo/obj/Debug/net8.0/ReciclaFuturo.AssemblyInfo.cs
+++ ReciclaFuturo/obj/Debug/net8.0/ReciclaFuturo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReciclaFuturo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d6f1122104f37fca6e83e75476b3f029afbb26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8097a3a02c4d07b453ac59d36235fe6b4080eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReciclaFuturo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReciclaFuturo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
