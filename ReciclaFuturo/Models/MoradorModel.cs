16c8fa4f46ece68317e8de3a8a7e4f47b04494d7
esterquintino <esterqdasilva@outlook.com>
2024-06-27 12:49:20 -0300

    Model Morador

---
+++ ReciclaFuturo/Models/MoradorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ReciclaFuturo.Models
+{
+    public class MoradorModel
+    {
+
+        public int MoradorId { get; set; }
+        public string? Nome { get; set; }
+        public int cpf { get; set; }
+        public string? email { get; set; }
+        public string? senha { get; set; }
+        public string? ContatoNr { get; set; }
+
+        public int EnderecoId { get; set; }
+        public EnderecoModel? Endereco { get; set; }
+
+    }
+}
