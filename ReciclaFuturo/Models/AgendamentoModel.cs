fc9c02dd8a061d0071491b8e894e898afbc304b1
esterquintino <esterqdasilva@outlook.com>
2024-06-27 12:59:44 -0300

    Model Agendamento

---
+++ ReciclaFuturo/Models/AgendamentoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ReciclaFuturo.Models
+{
+    public class AgendamentoModel
+    {
+        public int AgendamentoId { get; set; }
+        public DateTime? DataHoraAgendamento { get; set; }
+
+        public int MoradorId { get; set; }
+        public MoradorModel? Morador { get; set; }
+    }
+}
