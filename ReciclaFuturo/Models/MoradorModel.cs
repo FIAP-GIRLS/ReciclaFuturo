2e51754953049e7c0b8e51c6dd2f28768156bff5
Giulianna Oliveira <giuliannaodesouza@gmail.com>
2024-10-31 21:19:52 -0300

    Atualizações da model e controller de morador e endereço.

--- ReciclaFuturo/Models/MoradorModel.cs
+++ ReciclaFuturo/Models/MoradorModel.cs
@@ -4,32 +4,31 @@ namespace ReciclaFuturo.Models
 {
     public class MoradorModel
     {
-
         public int MoradorId { get; set; }
 
         [Required(ErrorMessage = "Nome é obrigatório")]
-        [StringLength(100, ErrorMessage = "Nome não pode ter mais de 64 caracteres")]
+        [StringLength(100, ErrorMessage = "Nome não pode ter mais de 100 caracteres")]
         public string? Nome { get; set; }
 
         [Required(ErrorMessage = "CPF é obrigatório")]
-        [RegularExpression(@"\d{11}", ErrorMessage = "CPF deve ter 11 dígitos")]
-        public int Cpf { get; set; }
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "CPF deve ter 11 dígitos")]
+        public string Cpf { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email é obrigatório")]
         [EmailAddress(ErrorMessage = "Email inválido")]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Senha é obrigatória")]
-        [StringLength(100, MinimumLength = 8, ErrorMessage = "Senha deve ter entre 8 e 16 caracteres")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Senha deve ter entre 8 e 100 caracteres")]
         public string? Senha { get; set; }
 
         [Phone(ErrorMessage = "Número de contato inválido")]
         public string? ContatoNr { get; set; }
 
         public int EnderecoId { get; set; }
-        public EnderecoModel? Endereco { get; set; }
+
+        public EnderecoModel? Endereco { get; set; } = new EnderecoModel();
 
         public List<AgendamentoModel> Agendamentos { get; set; } = new List<AgendamentoModel>();
-
     }
 }
